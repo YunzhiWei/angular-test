81aab364ab59bf122f2f2f128b91ea226d61d081
rails -v <yunzhi.wei@live.com>
2016-10-01 20:23:31 +0800

    Initial code.

---
+++ app.js
@@ -0,0 +1,63 @@
+(function () {
+'use strict';
+
+angular.module('NarrowItDownApp', [])
+.controller('NarrowItDownController', NarrowItDownController)
+.service('MenuSearchService', MenuSearchService)
+.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
+
+NarrowItDownController.$inject = ['MenuSearchService'];
+function NarrowItDownController (MenuSearchService) {
+  var narrowctrl = this;
+
+  narrowctrl.items = MenuSearchService.getItems();
+
+  narrowctrl.searchTerm = "";
+
+  narrowctrl.showMatchedMenuItems = function () {
+    console.log(narrowctrl.searchTerm);
+  }
+
+  // var promise = MenuSearchService.getMatchedMenuItems();
+  //
+  // promise.then(function (response) {
+  //   narrowctrl.found = response.data;
+  // })
+  // .catch(funtion (error) {
+  //   console.log("Something went terribly wrong!");
+  // });
+
+  // narrowctrl.found = MenuSearchService.getMatchedMenuItems();
+}
+
+MenuSearchService.$inject = ['$http', 'ApiBasePath']
+function MenuSearchService($http, ApiBasePath) {
+  var service = this;
+
+  var founditems = [
+    {
+      id: 877,
+      short_name: "A1",
+      name: "Won Ton Soup with Chicken",
+      description: "chicken-stuffed won ton",
+      price_small: 2.55,
+      price_large: 5
+    }
+  ];
+
+  service.getItems = function () {
+    return founditems;
+  };
+  //
+  // service.getMatchedMenuItems = function (searchTerm) {
+  //   return $http({
+  //     method: "GET",
+  //     url: (ApiBasePath + "/menu_items.json")
+  //   }).then(function (result) {
+  //     var founditems = result.data;
+  //     return founditems;
+  //   });
+  // }
+}
+
+})();
